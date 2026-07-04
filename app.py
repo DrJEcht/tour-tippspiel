c96545bb787c796e7471a2c3fda0f11bb8c15b83
JEchterhoff <108683271+DrJEcht@users.noreply.github.com>
2026-07-04 17:18:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -184,9 +184,12 @@ def ist_etappe_gesperrt(etappe):
     if not status:
         return False
 
-        if status.sperrzeit:
-            jetzt = datetime.now(ZoneInfo("Europe/Berlin")).replace(tzinfo=None)
-            return jetzt >= status.sperrzeit
+    if status.gesperrt:
+        return True
+
+    if status.sperrzeit:
+        jetzt = datetime.now(ZoneInfo("Europe/Berlin")).replace(tzinfo=None)
+        return jetzt >= status.sperrzeit
 
     return False
 
