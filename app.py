d504a49a7cef7e062b6551afbd130dcd3c29d85c
JEchterhoff <108683271+DrJEcht@users.noreply.github.com>
2026-07-05 07:42:40 +0200

    Aktualisieren von app.py

--- app.py
+++ app.py
@@ -332,7 +332,7 @@ def index():
     return render_template(
         "index.html",
         rangliste=rangliste,
-        kategorien=anzeige_kategorien,
+        kategorien=kategorien,,
         etappen=etappen,
         ausgewaehlte_etappe=ausgewaehlte_etappe,
         tipps_etappe=tipps_etappe,
